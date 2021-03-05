cf61b185bbf1c7c2417fb69556e4a55bb945d5d5
FusionTech <fusion@fusion.com>
2021-03-05 11:48:16 +0300

    feature: remove passwords from source code

---
+++ database/config/config.js
@@ -0,0 +1,19 @@
+// Use environment variables for config settings.
+// This is because secrets such as passwords should never be part of the source code
+// (and especially not committed to version control).
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.PROD_DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
--- database/models/index.js
+++ database/models/index.js
@@ -4,19 +4,20 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+const envConfigs = require('../config/config');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = envConfigs[env];
 const db = {};
 
-// let sequelize;
-// if (config.use_env_variable) {
-//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+let sequelize;
 
-const sequelize = new Sequelize(process.env.DEV_DATABASE_URL, config);
+if (config.url) {
+  sequelize = new Sequelize(config.url, config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
