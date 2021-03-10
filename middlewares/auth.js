e11d9ecd528a92f486d210c29e716d56a2fd492a
FusionTech <fusion@fusion.com>
2021-03-10 13:02:35 +0300

    feature: add invalid jwt error handler

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -16,7 +16,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
-    handleErr(err, req, res);
+    res.status(401).json({ message: err.message });
   }
 
   req.user = payload;
