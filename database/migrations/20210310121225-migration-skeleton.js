f305d4e5b87c7a6ce45cfbc3c9cf88cd2e0006d2
FusionTech <fusion@fusion.com>
2021-03-11 09:18:42 +0300

    fix: fix down in the last migration file

--- database/migrations/20210310121225-migration-skeleton.js
+++ database/migrations/20210310121225-migration-skeleton.js
@@ -6,6 +6,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+    await queryInterface.changeColumn('Users', 'dob', { type: Sequelize.STRING });
   }
 };
